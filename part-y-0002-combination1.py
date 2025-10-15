18461e75f5c3e926df071bba8ea40c968a43007d
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2025-10-15 20:12:24 +0300

    Create part-y-0002-combination1.py

---
+++ part-y-0002-combination1.py
@@ -0,0 +1,13 @@
+"""
+Students: x, y, z...
+
+We'll select two students as assistant club heads. Same club and same positions...
+
+x, y
+
+x, z, 
+
+y, z
+
+(If the problem instead required one "Lead Assistant Club Head" and one "Junior Assistant Club Head", the positions would be different, and it would become a Permutation problem.)
+ """
